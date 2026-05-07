9d9e5eeb4d2fe2698b594bfda7e7f978d46f563e
Shaheen Safi <ssafi9241@gmail.com>
2026-05-07 16:28:55 +0430

    Add all project files including payment gateways and dashboard

---
+++ src/app/(dashboard)/services/page.tsx
@@ -0,0 +1,11 @@
+"use client";
+import React from 'react';
+
+export default function ServicesPage() {
+  return (
+    <div className="p-8">
+      <h1 className="text-2xl font-bold">Services</h1>
+      <p>Our service list will be displayed here.</p>
+    </div>
+  );
+}
